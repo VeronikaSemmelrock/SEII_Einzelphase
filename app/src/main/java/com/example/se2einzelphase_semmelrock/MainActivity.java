df576dbc4a82388785d5ae9364dfa1fb8f0e5532
VeronikaSemmelrock <v1semmelro@edu.aau.at>
2021-03-11 22:39:24 +0100

    Working build. Deleted TCPClient. Tested and then corrected classes - task was not understood correctly in the first versions

--- app/src/main/java/com/example/se2einzelphase_semmelrock/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase_semmelrock/MainActivity.java
@@ -3,17 +3,28 @@ package com.example.se2einzelphase_semmelrock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    EditText inputtxt;
-    TextView outputtext;
-    Button btncalc;
-    TCPClient c;
+    public Button btn;
+    public EditText inputtxt;
+    public static TextView outputtext;
+    public Button btncalc;
+    private String output;
+    private String input;
+    private static TextView outputtextCalc;
 
 
 
@@ -25,31 +36,70 @@ public class MainActivity extends AppCompatActivity {
         inputtxt = findViewById(R.id.inputField);
         outputtext = findViewById(R.id.outputField);
         btncalc = findViewById(R.id.btncalc);
+        outputtextCalc = findViewById(R.id.txtoutputCalc);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try {
-                    c = new TCPClient();
-                    outputtext.setText(c.getOutputForUser());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            public void onClick(View v) { taskButton(); }
         });
 
         btncalc.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                c.setInputFromUser(inputtxt.getText().toString());
+            public void onClick(View v) { taskCalcButton();}
+        });
+    }
+
+    private void taskCalcButton() {
+        input = inputtxt.getText().toString();
+        if (input.length() >0){
+            output = "";
+            ArrayList<Integer> even = new ArrayList<Integer>();
+            ArrayList<Integer> uneven = new ArrayList<Integer>();
+
+            String[] chars = inputtxt.getText().toString().split("");
+            for(String c : chars){
+                if(Integer.parseInt(c) % 2 == 0){
+                    even.add(Integer.parseInt(c));
+                }else{
+                    uneven.add(Integer.parseInt(c));
+                }
+            }
+            Collections.sort(even);
+            Collections.sort(uneven);
+            for(int i : even){
+                output += i;
+            }
+            for(int i : uneven){
+                output += i;
+            }
+            outputtextCalc.setText(output);
+        }
+
+    }
+
+    private void taskButton() {
+        new Thread() {
+            public void run() {
+                String sending = inputtxt.getText().toString();
                 try {
-                    c.join();   //waits for thread to finish
-                } catch (InterruptedException e) {
+                    Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+                    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); //output attached to socket - so output to server
+                    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //input attached to socket - so input read from server
+                    outToServer.writeBytes(sending+"\n");
+                    String answer = inFromServer.readLine();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            outputtext.setText(answer);
+                        }
+                    });
+                    clientSocket.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-                outputtext.setText(c.getOutputForUser());
             }
-        });
+        }.start();
     }
 
 }
